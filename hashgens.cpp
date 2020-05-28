1a09349fb54fbd67ee8d0cc672ffdd33833cc085
Praneeth Kolichala <praneeth.kolichala@gmail.com>
2020-05-28 12:06:47 -0400

    Small optimizations

--- hashgens.cpp
+++ hashgens.cpp
@@ -1,6 +1,8 @@
 #include "hashgens.h"
 #include <cassert>
 
+
+// Scramble a 64-bit number
 static std::uint64_t murmur64(std::uint64_t h) {
 	h ^= h >> 33;
 	h *= 0xff51afd7ed558ccd;
@@ -10,19 +12,37 @@ static std::uint64_t murmur64(std::uint64_t h) {
 	return h;
 }
 
-static Element get3DistinctValues(std::uint64_t value, std::uint64_t max) {
-    Element e;
-    std::size_t h0 = murmur64(value), h1 = murmur64(h0), h2 = murmur64(h1);
-    std::size_t h1diff = 1 + (h1 % (max - 1));
-    std::size_t h2diff = 2 + (h2 % (max - 2));
+// Requires v to be a random int in the range [0, 2^32)
+// Computes a random integer in [0, m) based on v
+// Assumes m < 2^32
+static std::uint64_t reduce(std::uint64_t v, std::uint64_t m) {
+    return (v * m) >> 32;
+}
 
-    e.h0 = h0 % max;
+static const std::uint64_t MASK_32 = 0xFFFFFFFF;
+
+// Sets remaining bits to h3
+static Element get3DistinctValues(std::uint64_t value, std::uint64_t max, std::uint64_t& h3) {
+    Element e;
+    // std::size_t h0 = murmur64(value), h1 = murmur64(h0), h2 = murmur64(h1);
+    std::size_t h0 = value & MASK_32, h1 = value >> 32, mur = murmur64(value), h2 = mur & MASK_32;
+    h3 = mur >> 32;
+    std::size_t h1diff = 1 + reduce(h1, max - 1);
+    std::size_t h2diff = 2 + reduce(h2, max - 2);
+
+    e.h0 = reduce(h0, max);
     if (h2diff <= h1diff) --h2diff;
     e.h1 = e.h0 + h1diff;
     if (e.h1 >= max) e.h1 -= max;
     e.h2 = e.h0 + h2diff;
     if (e.h2 >= max) e.h2 -= max;
     return e;
+}
+
+// Creates a ``dummy'' h3 to dispose of the remaining bits
+static Element get3DistinctValues(std::uint64_t value, std::uint64_t max) {
+    std::size_t h3;
+    return get3DistinctValues(value, max, h3);
 }
 
 
@@ -38,7 +58,8 @@ HashGenSegmented::HashGenSegmented(std::uint64_t m, std::size_t numSegments) : m
 
 Element HashGenSegmented::get(std::uint64_t value) const {
     Element e;
-    std::size_t h0 = murmur64(value), h1 = murmur64(h0), h2 = murmur64(h1), h3 = murmur64(h2);
+    // std::size_t h0 = murmur64(value), h1 = murmur64(h0), h2 = murmur64(h1), h3 = murmur64(h2);
+    std::size_t h0 = value & MASK_32, h1 = value >> 32, mur = murmur64(value), h2 = mur & MASK_32, h3 = mur >> 32;
     std::size_t segStart = h0 % (m_numSegments - 2);
     e.h0 = segStart * segmentLength + (h1 % segmentLength);
     e.h1 = (segStart + 1) * segmentLength + (h2 % segmentLength);
@@ -48,9 +69,9 @@ Element HashGenSegmented::get(std::uint64_t value) const {
 
 Element HashGenLimBandwidth::get(std::uint64_t value) const {
     Element e;
-    std::size_t h0 = murmur64(value);
-    e = get3DistinctValues(h0, m_d);
-    std::size_t start = h0 % (maxHashValue - m_d + 1);
+    std::size_t h0;
+    e = get3DistinctValues(value, m_d, h0);
+    std::size_t start = reduce(h0, maxHashValue - m_d + 1);
     e.h0 += start;
     e.h1 += start;
     e.h2 += start;
